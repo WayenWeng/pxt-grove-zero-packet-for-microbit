293bbae77aa5da3ba74ed43f3eb08563effc28b7
jinyuan.weng <jinyuan.weng@seeed.cc>
2017-11-15 15:28:57 +0800

    Update

--- driver.ts
+++ driver.ts
@@ -61,7 +61,7 @@ namespace driver
                             control.raiseEvent(eventId, driver.lastStatus[address]);
                         }
                     }
-                    basic.pause(100);
+                    basic.pause(50);
                 }
             });
         }
