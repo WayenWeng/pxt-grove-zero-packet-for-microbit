658d9e23ba25697cd87dc8e3f01aff5c212dc559
jinyuan.weng <jinyuan.weng@seeed.cc>
2017-12-04 17:11:48 +0800

    Update

--- motor.ts
+++ motor.ts
@@ -3,31 +3,35 @@ enum MotorTpye {
     //% block=servo
     Servo = 0x24,
     //% block=wheel
-    Wheel = 0x26
+    Wheel = 0x28
 };
 
 enum SpeedTpye {
     //% block=slow
-    Slow = 0,
+    Slow = 1,
     //% block=medium
-    Medium = 1,
+    Medium = 2,
     //% block=fast
-    Fast = 2
+    Fast = 3
 };
 
 enum DirectionTpye {
-    //% block=left
-    Left = 0,
-    //% block=right
-    Right = 1,
     //% block=straight
-    Straight = 2,
+    Straight = 1,
     //% block=back
-    Back = 3
+    Back = 2,
+    //% block=left
+    Left = 3,
+    //% block=right
+    Right = 4,
+    //% block=clockwise
+    Clockwise = 5,
+    //% block=anticlockwise
+    Anticlockwise = 6
 };
 
 /**
- * Functions to operate Grove Zero device.
+ * Functions to operate G2 module.
  */
 //% weight=48 color=#A26236 icon="\uf018" block="Motor"
 namespace motor
@@ -56,7 +60,41 @@ namespace motor
     //% weight=99 blockGap=8
     export function runWheel(speed: SpeedTpye, direction: DirectionTpye)
     {
-
+        let left = 0, right = 0;
+        if(speed == SpeedTpye.Slow){left = 85; right = 85;}
+        else if(speed == SpeedTpye.Medium){left = 170; right = 170;}
+        else if(speed == SpeedTpye.Fast){left = 255; right = 255;}
+        
+        if(direction == DirectionTpye.Straight){}
+        else if(direction == DirectionTpye.Back){left = (-1) * left; right = (-1) * right;}
+        else if(direction == DirectionTpye.Left){left = 255 - left / 2; right = 255;}
+        else if(direction == DirectionTpye.Right){right = 255 - right / 2; left = 255;}
+        else if(direction == DirectionTpye.Clockwise){right = -right; }
+        else if(direction == DirectionTpye.Anticlockwise){left = -left; }
+        
+        runWheelWithDuty(left, right);
+    }
+    
+    /**
+     * Stop wheel run.
+     */
+    //% blockId=motor_stop_wheel block="wheel stop"
+    //% weight=98 blockGap=8
+    export function stopWheel()
+    {        
+        runWheelWithDuty(0, 0);
+    }
+    
+    /**
+     * Set the mini fan by a speed.
+     * @param speed the speed you want to set.
+     */
+    //% blockId=motor_set_mini_fan block="mini fan speed|%speed"
+    //% speed.min=0 speed.max=128 speed.defl=0
+    //% weight=97 blockGap=8
+    export function setMiniFan(speed: number)
+    {
+        
     }
     
     /**
@@ -71,6 +109,12 @@ namespace motor
     //% advanced=true
     export function runWheelWithDuty(left: number, right: number)
     {
-
+        let data: Buffer = pins.createBuffer(5);
+        data[0] = 0x01;
+        data[1] = left & 0xff;
+        data[2] = (left >> 8) & 0xff;
+        data[3] = right & 0xff;
+        data[4] = (right >> 8) & 0xff;
+        driver.i2cSendBytes(MotorTpye.Wheel, data);
     }
 }
